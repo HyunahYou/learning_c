0cbac436fd0881a463c7bc5a32608edcbbc579cc
HyunahYou <ryuh49@naver.com>
2020-01-04 01:05:20 +0900

    printf 함수의 주석처리

--- StudyCProgrammingFromBrother/main.c
+++ StudyCProgrammingFromBrother/main.c
@@ -5,9 +5,9 @@ int main(void) {
     int num3 = 032, num4 = 024;
     char carr[15] = "cats\0test";
     char *cptr = "dogs";
-    printf("%s\n", carr);
-    printf("%p\n", cptr);
-    printf("%s\n", cptr);
+//    printf("%s\n", carr);
+//    printf("%p\n", cptr);
+//    printf("%s\n", cptr);
 
     printNum(num1,num2,num3,num4);
 
