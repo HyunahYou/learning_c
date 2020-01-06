bb4ad38eec6367cbc80ea07d74ef36c376dcb3d5
HyunahYou <ryuh49@naver.com>
2020-01-06 23:43:42 +0900

    chapter04-3 예제2

--- StudyCProgrammingFromBrother/main.c
+++ StudyCProgrammingFromBrother/main.c
@@ -3,7 +3,7 @@
 int main(void) {
     int num1=15; // 00000000 00000000 00000000 00001111
     int num2=20; // 00000000 00000000 00000000 00010100
-    int num3=num1&num2; // num1과 num2의 비트단위 &연산=00000100=4
-    printf("AND 연산의 결과: %d\n", num3);
+    int num3=num1|num2; // 00011111=31
+    printf("OR 연산의 결과: %d\n", num3);
     return 0;
 }
