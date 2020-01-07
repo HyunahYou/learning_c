cb4e78cd8196c84af33e8e46f37119eceee10429
HyunahYou <ryuh49@naver.com>
2020-01-07 22:26:05 +0900

    chapter05-4 예제2

--- StudyCProgrammingFromBrother/src/chapter05.c
+++ StudyCProgrammingFromBrother/src/chapter05.c
@@ -1,13 +1,9 @@
 #include "stdio.h"
 
 int main(void) {
-    double num1=245;
-    int num2=3.1415;
-    int num3=129;
-    char ch=num3;
-
-    printf("정수 245를 실수로: %f\n", num1);
-    printf("실수 3.1415를 정수로: %d\n", num2);
-    printf("큰 정수 129를 작은 정수로: %d\n", ch);
+    int num1=3, num2=4;
+    double divResult;
+    divResult=num1/num2;
+    printf("나눗셈 결과: %f\n", divResult);
     return 0;
 }
