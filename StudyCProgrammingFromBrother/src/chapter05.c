c3ef23cbdace103b66af384c8e50b041135dadd6
HyunahYou <ryuh49@naver.com>
2020-01-07 21:27:33 +0900

    chapter05-3 예제1

--- StudyCProgrammingFromBrother/src/chapter05.c
+++ StudyCProgrammingFromBrother/src/chapter05.c
@@ -1,11 +1,8 @@
 #include "stdio.h"
 
 int main(void) {
-    char asc;
-
-    printf("하나의 알파벳 입력: ");
-    scanf("%c", &asc);
-
-    printf("해당 알파벳의 아스키 코드 값: %d\n", asc);
+    printf("literal int size: %d\n", sizeof(7));
+    printf("literal double size: %d\n", sizeof(7.14));
+    printf("literal char size: %d\n", sizeof('A'));
     return 0;
 }
