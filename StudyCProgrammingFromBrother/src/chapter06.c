df135aa1d74762471389e2916bd923e1c43f2967
HyunahYou <ryuh49@naver.com>
2020-01-07 22:49:05 +0900

    chapter06-1 예제3

--- StudyCProgrammingFromBrother/src/chapter06.c
+++ StudyCProgrammingFromBrother/src/chapter06.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 
 int main(void) {
-    int myAge=12;
-    printf("제 나이는 10진수로 %d살, 16진수로 %X살입니다.\n", myAge, myAge);
+    int num1=7, num2=13;
+    printf("%o %#o\n", num1, num1);
+    printf("%x %#x\n", num2, num2);
     return 0;
 }
 
