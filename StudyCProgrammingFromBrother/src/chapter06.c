00c2c2a2d4e1930146d15c7da82fecfd7f8d5fd2
HyunahYou <ryuh49@naver.com>
2020-01-07 23:25:19 +0900

    chapter06-2 예제2

--- StudyCProgrammingFromBrother/src/chapter06.c
+++ StudyCProgrammingFromBrother/src/chapter06.c
@@ -1,11 +1,20 @@
 #include <stdio.h>
 
 int main(void) {
-    int num1, num2, num3;
-    printf("세 개의 정수 입력: ");
-    scanf("%d %o %x", &num1, &num2, &num3);
+    float num1;
+    double num2;
+    long double num3;
 
-    printf("입력된 정수 10진수 출력: ");
-    printf("%d %d %d\n", num1, num2, num3);
+    printf("실수 입력1(e 표기법으로): ");
+    scanf("%f", &num1);
+    printf("입력된 실수 %f\n", num1);
+
+    printf("실수 입력2(e 표기법으로): ");
+    scanf("%lf", &num2);
+    printf("입력된 실수 %lf\n", num2);
+
+    printf("실수 입력3(e 표기법으로): ");
+    scanf("%Lf", &num3);
+    printf("%Lf\n", num3);
     return 0;
 }
