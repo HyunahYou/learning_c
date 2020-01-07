dc23c59859f0600f109831a71e5b3600054df12d
HyunahYou <ryuh49@naver.com>
2020-01-07 23:18:21 +0900

    chapter06-2 예제1

--- StudyCProgrammingFromBrother/src/chapter06.c
+++ StudyCProgrammingFromBrother/src/chapter06.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 
 int main(void) {
-    printf("%-8s  %14s  %5s\n", "이 름", "전공학과", "학년");
-    printf("%-8s  %14s %5d\n", "김동수", "전자공학", 3);
-    printf("%-8s  %14s %5d\n", "이을수", "컴퓨터공학", 2);
-    printf("%-8s  %14s %5d\n", "한선영", "미술교육학", 4);
+    int num1, num2, num3;
+    printf("세 개의 정수 입력: ");
+    scanf("%d %o %x", &num1, &num2, &num3);
+
+    printf("입력된 정수 10진수 출력: ");
+    printf("%d %d %d\n", num1, num2, num3);
     return 0;
 }
