19c7763b4a7d43ab0aa960f36bd833724fd6bedc
HyunahYou <ryuh49@naver.com>
2020-01-07 22:27:13 +0900

    chapter05-4 예제2-2

--- StudyCProgrammingFromBrother/src/chapter05.c
+++ StudyCProgrammingFromBrother/src/chapter05.c
@@ -3,7 +3,7 @@
 int main(void) {
     int num1=3, num2=4;
     double divResult;
-    divResult=num1/num2;
+    divResult=(double)num1/num2;
     printf("나눗셈 결과: %f\n", divResult);
     return 0;
 }
