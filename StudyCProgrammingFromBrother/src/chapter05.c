884fa16f5589b9f5e792d06c6604672ec1c63632
HyunahYou <ryuh49@naver.com>
2020-01-07 20:26:57 +0900

    chapter05-1 예제3

--- StudyCProgrammingFromBrother/src/chapter05.c
+++ StudyCProgrammingFromBrother/src/chapter05.c
@@ -1,17 +1,12 @@
 #include "stdio.h"
 
 int main(void) {
-    char num1=1, num2=2, result1=0;
-    short num3=300, num4=400, result2=0;
+    double rad;
+    double area;
+    printf("원의 반지름 입력: ");
+    scanf("%lf", &rad);
 
-    printf("size of num1 & num2: %d, %d\n", sizeof(num1), sizeof(num2));
-    printf("size of num3 & num4: %d, %d\n", sizeof(num3), sizeof(num4));
-
-    printf("size of char add: %d\n", sizeof(num1+num2));
-    printf("size of short add: %d\n", sizeof(num3+num4));
-
-    result1=num1+num2;
-    result2=num3+num4;
-    printf("size of result1 & result2: %d, %d\n", sizeof(result1), sizeof(result2));
+    area=rad*rad*3.1415;
+    printf("원의 넓이: %f\n\n", area);
     return 0;
 }
