51db67c9b866baa98c97c510a5c24746c98d4504
HyunahYou <ryuh49@naver.com>
2020-01-07 23:10:50 +0900

    chapter06-1 예제7

--- StudyCProgrammingFromBrother/src/chapter06.c
+++ StudyCProgrammingFromBrother/src/chapter06.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 
 int main(void) {
-    printf("%s, %s, %s\n", "AAA", "BBB", "CCC");
+    printf("%-8s  %14s  %5s\n", "이 름", "전공학과", "학년");
+    printf("%-8s  %14s %5d\n", "김동수", "전자공학", 3);
+    printf("%-8s  %14s %5d\n", "이을수", "컴퓨터공학", 2);
+    printf("%-8s  %14s %5d\n", "한선영", "미술교육학", 4);
     return 0;
 }
