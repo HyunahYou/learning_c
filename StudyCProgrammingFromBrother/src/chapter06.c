51addcabae708781198535cc2424be2b20738069
HyunahYou <ryuh49@naver.com>
2020-01-07 23:01:51 +0900

    chapter06-1 예제5

--- StudyCProgrammingFromBrother/src/chapter06.c
+++ StudyCProgrammingFromBrother/src/chapter06.c
@@ -1,9 +1,14 @@
 #include <stdio.h>
 
 int main(void) {
-    printf("%f\n", 0.1234);
-    printf("%e\n", 0.1234);
-    printf("%f\n", 0.12345678);
-    printf("%e\n", 0.12345678);
+    double d1=1.23e-3; //0.00123
+    double d2=1.23e-4; //0.000123
+    double d3=1.23e-5; //0.0000123
+    double d4=1.23e-6; //0.00000123
+
+    printf("%g\n", d1); //%f 스타일 출력
+    printf("%g\n", d2); //%f 스타일 출력
+    printf("%g\n", d3); //%e 스타일 출력
+    printf("%g\n", d4); //%e 스타일 출력
     return 0;
 }
