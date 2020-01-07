ff9465e2590a029249ae574124a2f92cf229649e
HyunahYou <ryuh49@naver.com>
2020-01-07 22:19:19 +0900

    chapter05-4 예제1

--- StudyCProgrammingFromBrother/src/chapter05.c
+++ StudyCProgrammingFromBrother/src/chapter05.c
@@ -1,8 +1,13 @@
 #include "stdio.h"
 
 int main(void) {
-    printf("literal int size: %d\n", sizeof(7));
-    printf("literal double size: %d\n", sizeof(7.14));
-    printf("literal char size: %d\n", sizeof('A'));
+    double num1=245;
+    int num2=3.1415;
+    int num3=129;
+    char ch=num3;
+
+    printf("정수 245를 실수로: %f\n", num1);
+    printf("실수 3.1415를 정수로: %d\n", num2);
+    printf("큰 정수 129를 작은 정수로: %d\n", ch);
     return 0;
 }
