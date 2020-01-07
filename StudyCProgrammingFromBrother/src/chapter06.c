1a6d90e5548650d48c82e3d3e137eee00968d9ba
HyunahYou <ryuh49@naver.com>
2020-01-07 22:43:32 +0900

    chapter06-1 예제2

--- StudyCProgrammingFromBrother/src/chapter06.c
+++ StudyCProgrammingFromBrother/src/chapter06.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
 
 int main(void) {
-    printf("I like programming\n");
-    printf("I love puppy!\n");
-    printf("I am so happy\n");
+    int myAge=12;
+    printf("제 나이는 10진수로 %d살, 16진수로 %X살입니다.\n", myAge, myAge);
     return 0;
 }
 
