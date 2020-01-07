7dafbf85f4b8dbe91ea8729d8aa97ada661162ff
HyunahYou <ryuh49@naver.com>
2020-01-07 20:13:58 +0900

    chapter05-1 예제2

--- StudyCProgrammingFromBrother/src/chapter05.c
+++ StudyCProgrammingFromBrother/src/chapter05.c
@@ -1,18 +1,17 @@
 #include "stdio.h"
 
 int main(void) {
-    char ch=9;
-    int inum=1052;
-    double dnum=3.1415;
-    printf("변수 ch의 크기: %d\n", sizeof(ch));
-    printf("변수 inum의 크기: %d\n", sizeof(inum));
-    printf("변수 dnum의 크기: %d\n", sizeof(dnum));
+    char num1=1, num2=2, result1=0;
+    short num3=300, num4=400, result2=0;
 
-    printf("char의 크기: %d\n", sizeof(char));
-    printf("int의 크기: %d\n", sizeof(int));
-    printf("long의 크기: %d\n", sizeof(long));
-    printf("long long의 크기: %d\n", sizeof(long long));
-    printf("float의 크기: %d\n", sizeof(float));
-    printf("double의 크기: %d\n", sizeof(double));
+    printf("size of num1 & num2: %d, %d\n", sizeof(num1), sizeof(num2));
+    printf("size of num3 & num4: %d, %d\n", sizeof(num3), sizeof(num4));
+
+    printf("size of char add: %d\n", sizeof(num1+num2));
+    printf("size of short add: %d\n", sizeof(num3+num4));
+
+    result1=num1+num2;
+    result2=num3+num4;
+    printf("size of result1 & result2: %d, %d\n", sizeof(result1), sizeof(result2));
     return 0;
 }
