915cc6149a8ac704f06d52a6d7fd46515dd93d3f
HyunahYou <ryuh49@naver.com>
2020-01-07 22:56:43 +0900

    chapter06-1 예제4

--- StudyCProgrammingFromBrother/src/chapter06.c
+++ StudyCProgrammingFromBrother/src/chapter06.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
 int main(void) {
-    int num1=7, num2=13;
-    printf("%o %#o\n", num1, num1);
-    printf("%x %#x\n", num2, num2);
+    printf("%f\n", 0.1234);
+    printf("%e\n", 0.1234);
+    printf("%f\n", 0.12345678);
+    printf("%e\n", 0.12345678);
     return 0;
 }
-
