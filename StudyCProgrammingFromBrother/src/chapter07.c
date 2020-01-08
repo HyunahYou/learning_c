5d0bd13c91ea2fddb510c558aeea0bd73d24a890
HyunahYou <ryuh49@naver.com>
2020-01-08 14:30:39 +0900

    chapter07-1 문제07-2 Q1

--- StudyCProgrammingFromBrother/src/chapter07.c
+++ StudyCProgrammingFromBrother/src/chapter07.c
@@ -1,16 +1,21 @@
 #include <stdio.h>
 
 int main(void) {
-    int cur=2;
-    int  is=0;
+    int cur=0, total=0;
+    int num;
+    num>=1;
 
-    while (cur<10) {
-        is=1;
-        while (is<10) {
-           printf("%d*%d=%d\n", cur, is, cur*is);
-           is++;
+    while (cur<5) {
+        printf("정수 입력: ");
+        scanf("%d", &num);
+        while (num<1) {
+            printf("정수 재입력: ");
+            scanf("%d", &num);
         }
+        total += num;
         cur++;
     }
+
+    printf("정수의 총합: %d\n", total);
     return 0;
 }
