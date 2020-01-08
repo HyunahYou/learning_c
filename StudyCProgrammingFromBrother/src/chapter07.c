c526f862ef49cbe3f33301aa09414207a5237cfc
HyunahYou <ryuh49@naver.com>
2020-01-08 14:12:13 +0900

    chapter07-1 예제3

--- StudyCProgrammingFromBrother/src/chapter07.c
+++ StudyCProgrammingFromBrother/src/chapter07.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 
 int main(void) {
-    int num1, num2=0, num3, total=0;
-    printf("입력할 정수의 갯수: ");
-    scanf("%d", &num1);
+    int cur=2;
+    int  is=0;
 
-    while (num2<num1) {
-        printf("정수 입력: ");
-        scanf("%d", &num3);
-        total+=num3;
-        num2++;
+    while (cur<10) {
+        is=1;
+        while (is<10) {
+           printf("%d*%d=%d\n", cur, is, cur*is);
+           is++;
+        }
+        cur++;
     }
-    printf("정수의 평균: %f\n", (double)total/num1); //★ %f ★
     return 0;
 }
