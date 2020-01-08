4b89341dc53e4b8b9d7cd14c02ab66caf27ee119
HyunahYou <ryuh49@naver.com>
2020-01-08 21:56:31 +0900

    chapter09-1 예제3

--- StudyCProgrammingFromBrother/src/chapter09.c
+++ StudyCProgrammingFromBrother/src/chapter09.c
@@ -4,11 +4,27 @@ int Add(int num1, int num2) {
 return num1+num2;
 }
 
+void ShowAddResult (int num) {
+    printf("덧셈결과 출력: %d \n", num);
+}
+
+int ReadNum(void) {
+    int num;
+    scanf("%d", &num);
+    return num;
+}
+
+void HowToUseThisProg(void) {
+    printf("두 개의 정수를 입력하시면 덧셈결과가 출력됩니다. \n");
+    printf("자! 그럼 두 개의 정수를 입력하세요. \n");
+}
+
 int main(void) {
-    int result;
-    result=Add(3,4);
-    printf("덧셈결과1: %d \n", result);
-    result=Add(5, 8);
-    printf("덧셈결과2: %d \n", result);
+    int result, num1, num2;
+    HowToUseThisProg();
+    num1=ReadNum();
+    num2=ReadNum();
+    result=Add(num1, num2);
+    ShowAddResult(result);
     return 0;
 }
