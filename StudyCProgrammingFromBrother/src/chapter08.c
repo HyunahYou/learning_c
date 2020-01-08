74aeaf30e8973ea229671497417173dce29a7ff2
HyunahYou <ryuh49@naver.com>
2020-01-08 19:28:57 +0900

    chapter08-1 문제08-1 Q3

--- StudyCProgrammingFromBrother/src/chapter08.c
+++ StudyCProgrammingFromBrother/src/chapter08.c
@@ -1,13 +1,21 @@
 #include <stdio.h>
 
 int main(void) {
-    int num1, num2;
+    int kor, eng, mat;
+    double ave;
 
-    printf("두 개의 정수 입력: ");
-    scanf("%d %d", &num1, &num2);
+    printf("국어, 영어, 수학 점수 입력: ");
+    scanf("%d, %d, %d", &kor, &eng, &mat);
+    ave=(kor+eng+mat)/3.0;
 
-    if (num1>num2)
-        printf("뺄셈 결과: %d\n", num1-num2);
-    else printf("뺄셈 결과: %d\n", num2-num1);
+    if (ave>=90.0)
+        printf("A\n");
+    else if (ave>=80.0)
+        printf("B\n");
+    else if (ave>=70.0)
+        printf("C\n");
+    else if (ave>=50.0)
+        printf("D\n");
+    else printf("F\n");
     return 0;
 }
