463b4248e8de3f06eeddcde407fe307f1e5d0c32
HyunahYou <ryuh49@naver.com>
2020-01-08 21:11:57 +0900

    chapter08-3 예제3

--- StudyCProgrammingFromBrother/src/chapter08.c
+++ StudyCProgrammingFromBrother/src/chapter08.c
@@ -1,21 +1,24 @@
 #include <stdio.h>
 
 int main(void) {
-    char num;
-    printf("0이상의 정수 입력: ");
+    int num;
+    printf("자연수 입력: ");
     scanf("%d", &num);
 
-    switch (num/10) {
-        case 0:
-            printf("0이상 10미만 \n");
-            break;
-        case 1:
-            printf("10이상 20미만 \n");
-            break;
-        case 2:
-            printf("20이상 30미만");
-        default:
-            printf("30이상 \n");
-    }
+    if (num==1)
+        goto ONE;
+    else if (num==2)
+        goto TWO;
+    else goto OTHER;
+
+    ONE:
+    printf("1을 입력하셨습니다! \n");
+    goto END;
+    TWO:
+    printf("2를 입력하셨습니다! \n");
+    goto END;
+    OTHER:
+    printf("3 혹은 다른 값을 입력하셨군요! \n");
+    END:
     return 0;
 }
