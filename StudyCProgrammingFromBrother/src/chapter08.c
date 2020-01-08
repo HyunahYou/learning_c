365abd3c807c4f78f18e963218c12abe12a79492
HyunahYou <ryuh49@naver.com>
2020-01-08 18:35:57 +0900

    chapter08-1 예제6

--- StudyCProgrammingFromBrother/src/chapter08.c
+++ StudyCProgrammingFromBrother/src/chapter08.c
@@ -1,23 +1,11 @@
 #include <stdio.h>
 
 int main(void) {
-    int opt;
-    double num1, num2;
-    double result;
+    int num, abs;
+    printf("정수 입력: ");
+    scanf("%d", &num);
 
-    printf("1.덧셈 2.뺄셈 3.곱셈 4.나눗셈\n");
-    printf("선택? ");
-    scanf("%d", &opt);
-    printf("두 개의 실수 입력: ");
-    scanf("%lf %lf", &num1, &num2);
-
-    if (opt==1)
-        result=num1+num2;
-    else if (opt==2)
-        result=num1-num2;
-    else if (opt==3)
-        result=num1*num2;
-    else result=num1/num2;
-    printf("결과: %f\n", result);
+    abs= num>0 ? num : num*(-1);
+    printf("절댓값: %d\n", abs);
     return 0;
 }
