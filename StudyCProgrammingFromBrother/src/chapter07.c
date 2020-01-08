eed090c9c26b00ccd4429b97c0a6101666e17fbe
HyunahYou <ryuh49@naver.com>
2020-01-08 15:48:17 +0900

    chapter07-3 예제1

--- StudyCProgrammingFromBrother/src/chapter07.c
+++ StudyCProgrammingFromBrother/src/chapter07.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
 
 int main(void) {
-    int cur = 2, is = 0;
-    do {
-        is = 1;
-        do {
-            printf("%d*%d=%d\n", cur, is, cur * is);
-            is++;
-        } while (is < 10);
-        cur++;
-    } while (cur < 10);
+    int total=0;
+    int i, num;
+    printf("0부터 num까지의 덧셈, num은? ");
+    scanf("%d", &num);
+
+    for (i=0; i<num+1; i++)
+        total+=i;
+    printf("0부터 %d까지 덧셈결과: %d\n", num, total);
     return 0;
 }
