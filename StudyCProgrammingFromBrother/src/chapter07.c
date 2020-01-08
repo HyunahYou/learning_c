90f1526a61d0d5e280cb27a92f0967eedce0a58c
HyunahYou <ryuh49@naver.com>
2020-01-08 15:30:08 +0900

    chapter07-2 문제07-3 Q2

--- StudyCProgrammingFromBrother/src/chapter07.c
+++ StudyCProgrammingFromBrother/src/chapter07.c
@@ -1,17 +1,11 @@
 #include <stdio.h>
 
 int main(void) {
-    int total=0, num=0;
-
-    printf("정수 입력(0 to quit): ");
-    scanf("%d", &num);
-    total+=num;
-
-    while (num!=0) {
-        printf("정수 입력(0 to quit): ");
-        scanf("%d", &num);
-        total+=num;
-    }
-    printf("합계: %d\n", total);
+    int total=0, num=1;
+    do {
+        total+=num*2;
+        num++;
+    } while (num<=50);
+    printf("0~100 중 짝수의 합계: %d\n", total);
     return 0;
 }
