e8c344302f69865f56b8f37ac5aa9dc98c0d0567
HyunahYou <ryuh49@naver.com>
2020-01-08 23:01:05 +0900

    chapter09-1 문제09-1 Q1

--- StudyCProgrammingFromBrother/src/chapter09.c
+++ StudyCProgrammingFromBrother/src/chapter09.c
@@ -1,24 +1,39 @@
 #include <stdio.h>
 
-int AbsoCompare(int num1, int num2);
-int GetAbsoValue (int num);
+int MCompare(int n1, int n2);
+int mCompare(int n1, int n2);
+int Max(int num1, int num2, int num3);
+int mini(int num1, int num2, int num3);
 
 int main(void) {
-    int num1, num2;
-    printf("두 개의 정수 입력: ");
-    scanf("%d %d", &num1, &num2);
-    printf("%d와 %d중 절댓값이 큰 정수: %d \n", num1, num2, AbsoCompare(num1, num2));
+    int num1, num2, num3;
+    printf("세 개의 정수 입력: ");
+    scanf("%d %d %d", &num1, &num2, &num3);
+    printf("%d, %d, %d중 가장 큰 수: %d \n", num1, num2, num3, Max(num1, num2, num3));
+    printf("%d, %d, %d중 가장 작은 수: %d \n", num1, num2, num3, mini(num1, num2, num3));
     return 0;
 }
 
-int AbsoCompare (int num1, int num2) {
-    if (GetAbsoValue(num1)>GetAbsoValue(num2))
-        return num1;
-    else return num2;
+int MCompare (int n1, int n2) {
+    if (n1>n2)
+        return n1;
+    else return n2;
 }
 
-int GetAbsoValue (int num) {
-    if (num<0)
-        return num*(-1);
-    else return num;
+int mCompare (int n1, int n2) {
+    if (n1<n2)
+        return n1;
+    else return n2;
+}
+
+int Max (int num1, int num2, int num3) {
+    if (MCompare(num1, num2) > MCompare(num2, num3))
+        return MCompare(num1, num2);
+    else return MCompare(num2, num3);
+}
+
+int mini (int num1, int num2, int num3) {
+    if (mCompare(num1, num2) < mCompare(num2, num3))
+        return mCompare(num1, num2);
+    else return mCompare(num2, num3);
 }
