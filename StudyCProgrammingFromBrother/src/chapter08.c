5dfdecd94ff5746debdc5a8648fd7b4be0830b12
HyunahYou <ryuh49@naver.com>
2020-01-08 17:27:00 +0900

    chapter08-1 예제2

--- StudyCProgrammingFromBrother/src/chapter08.c
+++ StudyCProgrammingFromBrother/src/chapter08.c
@@ -1,16 +1,25 @@
 #include <stdio.h>
 
 int main(void) {
-    int num;
-    printf("정수 입력: ");
-    scanf("%d", &num);
+    int opt;
+    double num1, num2;
+    double result;
 
-    if (num<0)
-        printf("입력 값은 0보다 작다.\n");
-    if(num>0)
-        printf("입력 값은 0보다 크다.\n");
-    if(num==0)
-        printf("입력 값은 0이다.\n");
+    printf("1.덧셈 2.뺄셈 3.곱셈 4.나눗셈\n");
+    printf("선택? ");
+    scanf("%d", &opt);
+    printf("두 개의 실수 입력: ");
+    scanf("%lf %lf", &num1, &num2);
 
+    if (opt==1)
+        result=num1+num2;
+    if (opt==2)
+        result=num1-num2;
+    if (opt==3)
+        result=num1*num2;
+    if (opt==4)
+        result=num1/num2;
+
+    printf("결과: %f\n", result);
     return 0;
 }
