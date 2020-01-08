df23299999f798bd47c2ff5c9ce246a726620fa4
HyunahYou <ryuh49@naver.com>
2020-01-08 22:30:57 +0900

    chapter09-1 예제5

--- StudyCProgrammingFromBrother/src/chapter09.c
+++ StudyCProgrammingFromBrother/src/chapter09.c
@@ -1,16 +1,24 @@
 #include <stdio.h>
 
-int NumberCompare (int num1, int num2);
+int AbsoCompare(int num1, int num2);
+int GetAbsoValue (int num);
 
 int main(void) {
-    printf("3과 4중에서 큰 수는 %d 이다. \n", NumberCompare(3, 4));
-    printf("7과 2중에서 큰 수는 %d 이다. \n", NumberCompare(7, 2));
+    int num1, num2;
+    printf("두 개의 정수 입력: ");
+    scanf("%d %d", &num1, &num2);
+    printf("%d와 %d중 절댓값이 큰 정수: %d \n", num1, num2, AbsoCompare(num1, num2));
     return 0;
 }
 
-int NumberCompare (int num1, int num2) {
-    if (num1>num2)
+int AbsoCompare (int num1, int num2) {
+    if (GetAbsoValue(num1)>GetAbsoValue(num2))
         return num1;
-    else
-        return num2;
+    else return num2;
+}
+
+int GetAbsoValue (int num) {
+    if (num<0)
+        return num*(-1);
+    else return num;
 }
