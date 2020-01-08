f8074283585ccc6dfb519ac23e62f7eb56e43013
HyunahYou <ryuh49@naver.com>
2020-01-08 15:17:27 +0900

    chapter07-2 문제07-3 Q1 방법1

--- StudyCProgrammingFromBrother/src/chapter07.c
+++ StudyCProgrammingFromBrother/src/chapter07.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
 int main(void) {
-    int total=0, num=0;
+    int total=0, num=1;
 
-    do {
+    while (num!=0) {
         printf("정수 입력(0 to quit): ");
         scanf("%d", &num);
         total+=num;
-    } while (num!=0);
+    }
     printf("합계: %d\n", total);
     return 0;
 }
