0777a44bbdc247bb8377ecb891b87398b3822176
HyunahYou <ryuh49@naver.com>
2020-01-08 15:19:06 +0900

    chapter07-2 문제07-3 Q1 방법2

--- StudyCProgrammingFromBrother/src/chapter07.c
+++ StudyCProgrammingFromBrother/src/chapter07.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
 
 int main(void) {
-    int total=0, num=1;
+    int total=0, num=0;
+
+    printf("정수 입력(0 to quit): ");
+    scanf("%d", &num);
+    total+=num;
 
     while (num!=0) {
         printf("정수 입력(0 to quit): ");
