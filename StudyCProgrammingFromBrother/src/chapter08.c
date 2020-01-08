c0b8bffdf8f67c3704b1a649be422ee4ac9cf3d2
HyunahYou <ryuh49@naver.com>
2020-01-08 17:32:37 +0900

    chapter08-1 예제3

--- StudyCProgrammingFromBrother/src/chapter08.c
+++ StudyCProgrammingFromBrother/src/chapter08.c
@@ -1,25 +1,11 @@
 #include <stdio.h>
 
 int main(void) {
-    int opt;
-    double num1, num2;
-    double result;
+    int num;
 
-    printf("1.덧셈 2.뺄셈 3.곱셈 4.나눗셈\n");
-    printf("선택? ");
-    scanf("%d", &opt);
-    printf("두 개의 실수 입력: ");
-    scanf("%lf %lf", &num1, &num2);
-
-    if (opt==1)
-        result=num1+num2;
-    if (opt==2)
-        result=num1-num2;
-    if (opt==3)
-        result=num1*num2;
-    if (opt==4)
-        result=num1/num2;
-
-    printf("결과: %f\n", result);
+    for (num = 1; num < 100; num++) {
+        if (num%3==0 || num%4==0)
+            printf("3 또는 4의 배수: %d\n", num);
+    }
     return 0;
 }
