d5986342f34dc77d8168dc278c42d0a04165f5ee
HyunahYou <ryuh49@naver.com>
2020-01-08 13:30:09 +0900

    chapter07-1 문제07-1 Q2

--- StudyCProgrammingFromBrother/src/chapter07.c
+++ StudyCProgrammingFromBrother/src/chapter07.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
 int main(void) {
-    int num1=0;
+    int num1=1;
     int num2;
-    printf("양의 정수 입력: ");
+    printf("배수의 갯수: ");
     scanf("%d", &num2);
 
-    while (num1<num2) {
-        printf("Hello world!\n");
+    while (num1<num2+1) {
+        printf("%d ", 3*num1);
         num1++;
     }
     return 0;
