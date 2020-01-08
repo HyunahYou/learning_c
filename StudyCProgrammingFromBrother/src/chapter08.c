6e96fd45286651a954b1fcf689688e4317c8b676
HyunahYou <ryuh49@naver.com>
2020-01-08 19:15:55 +0900

    chapter08-1 문제08-1 Q2

--- StudyCProgrammingFromBrother/src/chapter08.c
+++ StudyCProgrammingFromBrother/src/chapter08.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 
 int main(void) {
-    int num;
+    int num1, num2;
 
-    for (num=1; num<100; num++) {
-        if (num%7==0 || num%9==0)
-            printf("%d\n", num);
-    }
+    printf("두 개의 정수 입력: ");
+    scanf("%d %d", &num1, &num2);
+
+    if (num1>num2)
+        printf("뺄셈 결과: %d\n", num1-num2);
+    else printf("뺄셈 결과: %d\n", num2-num1);
     return 0;
 }
