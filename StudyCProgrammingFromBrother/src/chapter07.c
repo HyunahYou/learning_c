e7fd1daf2f9098017104a51cf7d0e2f5871a17eb
HyunahYou <ryuh49@naver.com>
2020-01-08 15:03:36 +0900

    chapter07-1 문제07-2 Q2(★또 보기★)

--- StudyCProgrammingFromBrother/src/chapter07.c
+++ StudyCProgrammingFromBrother/src/chapter07.c
@@ -1,17 +1,16 @@
 #include <stdio.h>
 
 int main(void) {
-    int cur=0, total=0, num=0;
+    int k=0, num=0;
 
-    while (cur<5) {
-        while (num<=0) {
-            printf("0보다 큰 수를 입력(%d번째): ", cur+1);
-            scanf("%d", &num);
+    while (k<5) {
+        while (num<k) {
+            printf("o ");
+            num++;
         }
-        total += num;
         num=0;
-        cur++;
+        printf("*\n");
+        k++;
     }
-    printf("총합: %d\n", total);
     return 0;
 }
