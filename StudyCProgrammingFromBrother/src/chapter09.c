eb8fe193a818b26e5810d98ed45b24d9f0739ebb
HyunahYou <ryuh49@naver.com>
2020-01-10 00:09:57 +0900

    chapter09-3 예제3

--- StudyCProgrammingFromBrother/src/chapter09.c
+++ StudyCProgrammingFromBrother/src/chapter09.c
@@ -1,17 +1,15 @@
 #include <stdio.h>
 
-int Add (int val);
-int num=1;
-
-int main (void) {
-    int num=5;
-    printf("num: %d\n", Add(3));
-    printf("num: %d\n", num+9);
-    return 0;
+void SimpleFunc (void) {
+    static int num1=0;
+    int num2=0;
+    num1++, num2++;
+    printf("static: %d, local: %d\n", num1, num2);
 }
 
-int Add (int val) {
-    int num=9;
-    num+=val;
-    return num;
+int main (void) {
+    int i;
+    for (i=0; i<3; i++)
+        SimpleFunc();
+    return 0;
 }
--- StudyCProgrammingFromBrother/src/main.c
+++ StudyCProgrammingFromBrother/src/main.c
@@ -1,20 +1,3 @@
-//09-3 예제2
-//int Add (int val);
-//int num=1;
-//
-//int main (void) {
-//    int num=5;
-//    printf("num: %d\n", Add(3));
-//    printf("num: %d\n", num+9);
-//    return 0;
-//}
-//
-//int Add (int val) {
-//    int num=9;
-//    num+=val;
-//    return num;
-//}
-
 //09-3 예제3
 //void SimpleFunc (void) {
 //    static int num1=0;
