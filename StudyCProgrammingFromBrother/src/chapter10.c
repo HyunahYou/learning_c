2e7f74d33bf3c1e49d3a0747954b46f7b74ab919
HyunahYou <ryuh49@naver.com>
2020-01-10 01:56:02 +0900

    chapter10 도전8

--- StudyCProgrammingFromBrother/src/chapter10.c
+++ StudyCProgrammingFromBrother/src/chapter10.c
@@ -1,17 +1,16 @@
 #include <stdio.h>
 
+int tn (int n) {
+    if (n==0)
+        return 1;
+    else return 2*tn(n-1);
+}
 int main (void) {
-    int n, k;
+    int n;
 
-    printf("입력: ");
+    printf("구하려는 2의 n제곱의 n 입력: ");
     scanf("%d", &n);
+    printf("2의 %d제곱: %d\n", n, tn(n));
 
-    if (n==1)
-        printf("k=0");
-    else { for (k=0; n>1; k++) {
-            n /= 2;
-        }
-        printf("k=%d\n", k);
-    }
     return 0;
 }
