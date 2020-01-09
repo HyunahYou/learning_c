9554ec17f935a56313db45e3e8f78c11ec3bfe90
HyunahYou <ryuh49@naver.com>
2020-01-10 00:19:21 +0900

    chapter10 도전2

--- StudyCProgrammingFromBrother/src/chapter10.c
+++ StudyCProgrammingFromBrother/src/chapter10.c
@@ -1,9 +1,22 @@
 #include <stdio.h>
 
 int main (void) {
-    int num;
-    printf("정수 입력: ");
-    scanf("%d", &num);
-    printf("%d의 16진수 값: %X\n", num, num);
+
+    int dan1, dan2, is;
+
+    printf("구구단을 출력할 두 개의 단 입력: ");
+    scanf("%d, %d", &dan1, &dan2);
+
+    if (dan1<dan2) {
+        for (; dan1<=dan2; dan1++) {
+            for (is=1; is<10; is++)
+                printf("%d*%d=%d\n", dan1, is, dan1*is);
+            printf("\n");
+        }
+    } else for (; dan2<=dan1; dan2++) {
+            for (int is=1; is<10; is++)
+                printf("%d*%d=%d\n", dan2, is, dan2*is);
+            printf("\n");
+        }
     return 0;
 }
