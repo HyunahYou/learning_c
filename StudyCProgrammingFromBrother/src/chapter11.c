b8b291eefb83c1c6ec1abd50d068225a241cc491
HyunahYou <ryuh49@naver.com>
2020-01-10 15:33:06 +0900

    chapter11-1 문제11-1 Q1(답지.ver)

--- StudyCProgrammingFromBrother/src/chapter11.c
+++ StudyCProgrammingFromBrother/src/chapter11.c
@@ -2,41 +2,23 @@
 
 int main (void) {
     int arr[5];
-    int sum=0;
+    int max, min, sum, i;
 
-    printf("5개의 정수 입력: ");
-    scanf("%d %d %d %d %d", &arr[0], &arr[1], &arr[2], &arr[3], &arr[4]);
-
-    printf("입력된 정수 중 최댓값: ");
-    if (arr[0]>arr[1] && arr[0]>arr[2] && arr[0]>arr[3]&& arr[0]>arr[4])
-        printf("%d", arr[0]);
-    else if (arr[1]>arr[0] && arr[1]>arr[2] && arr[1]>arr[3]&& arr[1]>arr[4])
-        printf("%d", arr[1]);
-    else if (arr[2]>arr[0] && arr[2]>arr[1] && arr[2]>arr[3]&& arr[2]>arr[4])
-        printf("%d", arr[2]);
-    else if (arr[3]>arr[0] && arr[3]>arr[1] && arr[3]>arr[2]&& arr[3]>arr[4])
-        printf("%d", arr[3]);
-    else if (arr[4]>arr[0] && arr[4]>arr[1] && arr[4]>arr[2]&& arr[4]>arr[3])
-        printf("%d", arr[4]);
-    printf("\n");
-
-    printf("입력된 정수 중 최솟값: ");
-    if (arr[0]<arr[1] && arr[0]<arr[2] && arr[0]<arr[3]&& arr[0]<arr[4])
-        printf("%d", arr[0]);
-    else if (arr[1]<arr[0] && arr[1]<arr[2] && arr[1]<arr[3]&& arr[1]<arr[4])
-        printf("%d", arr[1]);
-    else if (arr[2]<arr[0] && arr[2]<arr[1] && arr[2]<arr[3]&& arr[2]<arr[4])
-        printf("%d", arr[2]);
-    else if (arr[3]<arr[0] && arr[3]<arr[1] && arr[3]<arr[2]&& arr[3]<arr[4])
-        printf("%d", arr[3]);
-    else if (arr[4]<arr[0] && arr[4]<arr[1] && arr[4]<arr[2]&& arr[4]<arr[3])
-        printf("%d", arr[4]);
-    printf("\n");
-
-    for (int i=0; i<5; i++) {
-        sum+=arr[i];
+    for (i=0; i<5; i++) {
+        printf("입력: ");
+        scanf("%d", &arr[i]);
     }
-    printf("입력된 정수의 총합: %d\n", sum);
 
+    max=min=sum=arr[0];
+    for (i=1; i<5; i++) {
+        sum+=arr[i];
+        if (max<arr[i])
+            max=arr[i];
+        if (min>arr[i])
+            min=arr[i];
+    }
+    printf("최댓값: %d\n", max);
+    printf("최솟값: %d\n", min);
+    printf("총 합: %d", sum);
     return 0;
 }
