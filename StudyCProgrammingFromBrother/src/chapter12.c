5d0dd424684f8899549f80e4e86739d24bcb2fc2
HyunahYou <ryuh49@naver.com>
2020-01-10 21:42:01 +0900

    chapter12-2 문제12-1 Q2

--- StudyCProgrammingFromBrother/src/chapter12.c
+++ StudyCProgrammingFromBrother/src/chapter12.c
@@ -1,12 +1,17 @@
 #include <stdio.h>
 
 int main (void) {
-    int num=10;
-    int*ptr1=&num;
-    int*ptr2=ptr1;
+    int num1=10, num2=20;
+    int*ptr1=&num1;
+    int*ptr2=&num2;
 
-    (*ptr1)++;
-    (*ptr2)++;
-    printf("%d\n", num);
+    (*ptr1)+=10;
+    (*ptr2)-=10;
+
+    ptr1=&num2;
+    ptr2=&num1;
+
+    printf("ptr1이 가르키는 변수에 저장된 값: %d\n", *ptr1);
+    printf("ptr2이 가르키는 변수에 저장된 값: %d\n", *ptr2);
     return 0;
 }
