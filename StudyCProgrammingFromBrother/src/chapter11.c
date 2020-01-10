bab2d738c3f67b626c864f8b7a54e9d94e7f13fb
HyunahYou <ryuh49@naver.com>
2020-01-10 16:06:22 +0900

    chapter11-2 예제1

--- StudyCProgrammingFromBrother/src/chapter11.c
+++ StudyCProgrammingFromBrother/src/chapter11.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
 
 int main (void) {
-    char arr[]={"Good time"};
-    int arLen;
+    char str[]="Good morning!";
+    printf("배열 str의 크기: %d\n", sizeof(str));
+    printf("널 문자 문자형 출력: %c\n", str[13]);
+    printf("널 문자 정수형 출력: %d\n", str[13]);
 
-    arLen= sizeof(arr)/sizeof(char);
-
-    for (int i=0; i<arLen; i++)
-        printf("%c", arr[i]);
-    printf("\n");
+    str[12]='?';
+    printf("문자열 출력: %s\n", str);
     return 0;
 }
