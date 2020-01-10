66679ef8b780990c3b4c2e090ce03323370366e4
HyunahYou <ryuh49@naver.com>
2020-01-10 15:56:17 +0900

    chapter11-1 문제11-1 Q2

--- StudyCProgrammingFromBrother/src/chapter11.c
+++ StudyCProgrammingFromBrother/src/chapter11.c
@@ -1,24 +1,13 @@
 #include <stdio.h>
 
 int main (void) {
-    int arr[5];
-    int max, min, sum, i;
+    char arr[]={"Good time"};
+    int arLen;
 
-    for (i=0; i<5; i++) {
-        printf("입력: ");
-        scanf("%d", &arr[i]);
-    }
+    arLen= sizeof(arr)/sizeof(char);
 
-    max=min=sum=arr[0];
-    for (i=1; i<5; i++) {
-        sum+=arr[i];
-        if (max<arr[i])
-            max=arr[i];
-        if (min>arr[i])
-            min=arr[i];
-    }
-    printf("최댓값: %d\n", max);
-    printf("최솟값: %d\n", min);
-    printf("총 합: %d", sum);
+    for (int i=0; i<arLen; i++)
+        printf("%c", arr[i]);
+    printf("\n");
     return 0;
 }
