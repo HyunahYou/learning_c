d939ef02d961ce9df032bb8d57b569a82d123baa
HyunahYou <ryuh49@naver.com>
2020-01-10 19:52:18 +0900

    chapter11-2 문제11-2 Q3

--- StudyCProgrammingFromBrother/src/chapter11.c
+++ StudyCProgrammingFromBrother/src/chapter11.c
@@ -2,16 +2,20 @@
 
 int main (void) {
     char word[50];
-    int idx=0, len=0;
+    int len=0;
+    int i;
+    char max=0;
 
     printf("영단어 입력: ");
     scanf("%s", word);
 
-    for (len=0; word[idx]!='\0'; idx++)
-        len=idx;
-    printf("역순: ");
-    for (idx=len; idx>=0; idx--)
-        printf("%c", word[idx]);
+    for (; word[len]!='\0'; len++);
+    printf("아스키 코드 값이 가장 큰 문자: ");
+    for (i=0; i<len; i++) {
+        if (max<word[i])
+            max=word[i];
+    }
+    printf("%c", max);
 
     return 0;
 }
