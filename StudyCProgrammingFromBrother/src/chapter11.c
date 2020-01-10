7a6c1da026c5ca57b9c07e4a58335c980c218f40
HyunahYou <ryuh49@naver.com>
2020-01-10 19:08:11 +0900

    chapter11-2 문제11-2 Q2

--- StudyCProgrammingFromBrother/src/chapter11.c
+++ StudyCProgrammingFromBrother/src/chapter11.c
@@ -2,12 +2,16 @@
 
 int main (void) {
     char word[50];
-    int idx=0;
+    int idx=0, len=0;
 
     printf("영단어 입력: ");
     scanf("%s", word);
-    printf("글자 수: ");
-    for (; word[idx]!='\0'; idx++);
-    printf("%d", idx);
+
+    for (len=0; word[idx]!='\0'; idx++)
+        len=idx;
+    printf("역순: ");
+    for (idx=len; idx>=0; idx--)
+        printf("%c", word[idx]);
+
     return 0;
 }
