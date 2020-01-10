f9d9a6357e4fec680f356b662f073360210c1c69
HyunahYou <ryuh49@naver.com>
2020-01-10 21:33:29 +0900

    chapter12-2 문제12-1 Q1

--- StudyCProgrammingFromBrother/src/chapter12.c
+++ StudyCProgrammingFromBrother/src/chapter12.c
@@ -1,14 +1,12 @@
 #include <stdio.h>
 
 int main (void) {
-    int num1=100, num2=100;
-    int*pnum;
+    int num=10;
+    int*ptr1=&num;
+    int*ptr2=ptr1;
 
-    pnum=&num1;
-    (*pnum)+=30;
-    pnum=&num2;
-    (*pnum)-=30;
-
-    printf("num1:%d, num2:%d\n", num1, num2);
+    (*ptr1)++;
+    (*ptr2)++;
+    printf("%d\n", num);
     return 0;
 }
