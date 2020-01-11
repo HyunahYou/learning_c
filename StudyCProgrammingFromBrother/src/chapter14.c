93699d8a1a88de55cb06926086e9a6f7dfa45f58
HyunahYou <ryuh49@naver.com>
2020-01-11 19:55:12 +0900

    chapter14-1 예제2

--- StudyCProgrammingFromBrother/src/chapter14.c
+++ StudyCProgrammingFromBrother/src/chapter14.c
@@ -7,10 +7,21 @@ void ShowArayElem (int*param, int len) {
     printf("\n");
 }
 
+void AddArayElem(int*param, int len, int add) {
+    int i;
+    for (i=0; i<len; i++)
+        param[i]+=add;
+}
+
 int main (void) {
-    int arr1[3]={1, 2, 3};
-    int arr2[5]={4, 5, 6, 7, 8};
-    ShowArayElem(arr1, sizeof(arr1)/ sizeof(int));
-    ShowArayElem(arr2, sizeof(arr2)/ sizeof(int));
+    int arr[3]={1, 2, 3};
+    AddArayElem(arr, sizeof(arr)/ sizeof(int), 1);
+    ShowArayElem(arr, sizeof(arr)/ sizeof(int));
+
+    AddArayElem(arr, sizeof(arr)/ sizeof(int), 2);
+    ShowArayElem(arr, sizeof(arr)/ sizeof(int));
+
+    AddArayElem(arr, sizeof(arr)/ sizeof(int), 3);
+    ShowArayElem(arr, sizeof(arr)/ sizeof(int));
     return 0;
 }
