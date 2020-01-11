d98797a9e0a4a52d247455ecae7af377a8347d54
HyunahYou <ryuh49@naver.com>
2020-01-11 19:43:35 +0900

    chapter13-4 예제2

--- StudyCProgrammingFromBrother/src/chapter13.c
+++ StudyCProgrammingFromBrother/src/chapter13.c
@@ -1,11 +1,9 @@
 #include <stdio.h>
 
 int main (void) {
-    int num1=10, num2=20, num3=30;
-    int*arr[3]={&num1, &num2, &num3};
-
-    printf("%d\n", *arr[0]);
-    printf("%d\n", *arr[1]);
-    printf("%d\n", *arr[2]);
+    char*strArr[3]={"Simple", "String", "Array"};
+    printf("%s\n", strArr[0]);
+    printf("%s\n", strArr[1]);
+    printf("%s\n", strArr[2]);
     return 0;
 }
