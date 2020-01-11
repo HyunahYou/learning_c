7558e925ac111af3711612b016a53db7d59c7e5d
HyunahYou <ryuh49@naver.com>
2020-01-11 14:14:27 +0900

    chapter13-1 예제3

--- StudyCProgrammingFromBrother/src/chapter13.c
+++ StudyCProgrammingFromBrother/src/chapter13.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 int main (void) {
-    int arr1[3]={1, 2, 3};
-    double arr2[3]={1.1, 2.2, 3.3};
+    int arr[3]={15, 25, 35};
+    int*ptr=&arr[0];
 
-    printf("%d %g\n", *arr1, *arr2);
-    *arr1+=100;
-    *arr2+=120.5;
-    printf("%d %g\n", arr1[0], arr2[0]);
+    printf("%d %d\n", ptr[0], arr[0]);
+    printf("%d %d\n", ptr[1], arr[1]);
+    printf("%d %d\n", ptr[2], arr[2]);
+    printf("%d %d\n", *ptr, *arr);
     return 0;
 }
