aabbff8a113d91eb6e69e942a0f14bcce0bda7d6
HyunahYou <ryuh49@naver.com>
2020-01-11 20:01:46 +0900

    chapter14-2 예제2

--- StudyCProgrammingFromBrother/src/chapter14.c
+++ StudyCProgrammingFromBrother/src/chapter14.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
 
-void Swap (int n1, int n2) {
-    int temp=n1;
-    n1=n2;
-    n2=temp;
-    printf("n1 n2: %d %d\n", n1, n2);
+void Swap (int*ptr1, int*ptr2) {
+    int temp=*ptr1;
+    *ptr1=*ptr2;
+    *ptr2=temp;
 }
 
 int main (void) {
@@ -12,7 +11,7 @@ int main (void) {
     int num2=20;
     printf("num1 num2: %d %d\n", num1, num2);
 
-    Swap(num1, num2);
+    Swap(&num1, &num2);
     printf("num1 num2: %d %d\n", num1, num2);
     return 0;
 }
