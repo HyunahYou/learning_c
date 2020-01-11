dc6f281de97270e94121eb0f0077758e81bae770
HyunahYou <ryuh49@naver.com>
2020-01-11 18:22:47 +0900

    chapter13-2 문제13-1 Q2

--- StudyCProgrammingFromBrother/src/chapter13.c
+++ StudyCProgrammingFromBrother/src/chapter13.c
@@ -4,8 +4,7 @@ int main (void) {
     int arr[5]={1, 2, 3, 4, 5};
     int *ptr=arr;
     for (int i=0; i<5; i++) {
-        *ptr += 2;
-        ptr++;
+        *(ptr+i) += 2;
     }
     for (int i=0; i<5; i++) {
         printf("%d ", arr[i]);
