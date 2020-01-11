f4c960d33eed5b19d0c1de8e33df4f30884a149f
HyunahYou <ryuh49@naver.com>
2020-01-11 22:21:31 +0900

    chapter14-2 문제14-1 Q2

--- StudyCProgrammingFromBrother/src/chapter14.c
+++ StudyCProgrammingFromBrother/src/chapter14.c
@@ -1,16 +1,14 @@
 #include <stdio.h>
 
-int SquareByValue (int num) {
-    return num*num;
-}
-void SquareByReference (int*ptr) {
-    int num=*ptr;
-    *ptr=num*num;
+void Swap3 (int*ptr1, int*ptr2, int*ptr3) {
+    int temp=*ptr1;
+    *ptr1=*ptr3;
+    *ptr3=*ptr2;
+    *ptr2=temp;
 }
 int main (void) {
-    int num=10;
-    printf("%d\n", SquareByValue(num));
-    SquareByReference(&num);
-    printf("%d\n", num);
+    int num1=10, num2=20, num3=30;
+    Swap3(&num1, &num2, &num3);
+    printf("%d %d %d\n", num1, num2, num3);
     return 0;
 }
