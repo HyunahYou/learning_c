b306ea9e422a3b9ebee588ea5df68a273690a4bb
HyunahYou <ryuh49@naver.com>
2020-01-11 19:59:07 +0900

    chapter14-2 예제1

--- StudyCProgrammingFromBrother/src/chapter14.c
+++ StudyCProgrammingFromBrother/src/chapter14.c
@@ -1,27 +1,18 @@
 #include <stdio.h>
 
-void ShowArayElem (int*param, int len) {
-    int i;
-    for (i=0; i<len; i++)
-        printf("%d ", param[i]);
-    printf("\n");
-}
-
-void AddArayElem(int*param, int len, int add) {
-    int i;
-    for (i=0; i<len; i++)
-        param[i]+=add;
+void Swap (int n1, int n2) {
+    int temp=n1;
+    n1=n2;
+    n2=temp;
+    printf("n1 n2: %d %d\n", n1, n2);
 }
 
 int main (void) {
-    int arr[3]={1, 2, 3};
-    AddArayElem(arr, sizeof(arr)/ sizeof(int), 1);
-    ShowArayElem(arr, sizeof(arr)/ sizeof(int));
+    int num1=10;
+    int num2=20;
+    printf("num1 num2: %d %d\n", num1, num2);
 
-    AddArayElem(arr, sizeof(arr)/ sizeof(int), 2);
-    ShowArayElem(arr, sizeof(arr)/ sizeof(int));
-
-    AddArayElem(arr, sizeof(arr)/ sizeof(int), 3);
-    ShowArayElem(arr, sizeof(arr)/ sizeof(int));
+    Swap(num1, num2);
+    printf("num1 num2: %d %d\n", num1, num2);
     return 0;
 }
