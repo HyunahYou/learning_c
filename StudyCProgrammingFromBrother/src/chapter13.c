8961b4c3825f18f036708781776d6494c3edfc01
HyunahYou <ryuh49@naver.com>
2020-01-11 19:33:25 +0900

    chapter13-3 예제1

--- StudyCProgrammingFromBrother/src/chapter13.c
+++ StudyCProgrammingFromBrother/src/chapter13.c
@@ -1,18 +1,15 @@
 #include <stdio.h>
 
 int main (void) {
-    int arr[6]={1, 2, 3, 4, 5, 6};
-    int *ptr1=&arr[0];
-    int *ptr2=&arr[5];
-    int temp;
-    for (int i=0; i<3; i++) {
-        temp=*ptr1;
-        *ptr1=*ptr2;
-        *ptr2=temp;
-        ptr1+=1;
-        ptr2-=1;
-    }
-    for (int i=0; i<6; i++)
-        printf("%d ", arr[i]);
+    char str1[]="My String";
+    char*str2="Your String";
+    printf("%s %s\n", str1, str2);
+
+    str2="Our String";
+    printf("%s %s\n", str1, str2);
+
+    str1[0]='X';
+    str2[0]='X';
+    printf("%s %s\n", str1, str2);
     return 0;
 }
