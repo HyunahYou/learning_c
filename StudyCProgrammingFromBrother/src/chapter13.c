1e91e2bf99d3642ee39b2788dfc84edf00efc888
HyunahYou <ryuh49@naver.com>
2020-01-11 18:42:25 +0900

    chapter13-2 문제13-1 Q4

--- StudyCProgrammingFromBrother/src/chapter13.c
+++ StudyCProgrammingFromBrother/src/chapter13.c
@@ -1,13 +1,18 @@
 #include <stdio.h>
 
 int main (void) {
-    int arr[5]={1, 2, 3, 4, 5};
-    int *ptr=&arr[4];
-    int total=0;
-    for (int i=0; i<5; i++) {
-        total+=*ptr;
-        ptr--;
+    int arr[6]={1, 2, 3, 4, 5, 6};
+    int *ptr1=&arr[0];
+    int *ptr2=&arr[5];
+    int temp;
+    for (int i=0; i<3; i++) {
+        temp=*ptr1;
+        *ptr1=*ptr2;
+        *ptr2=temp;
+        ptr1+=1;
+        ptr2-=1;
     }
-    printf("total=%d", total);
+    for (int i=0; i<6; i++)
+        printf("%d ", arr[i]);
     return 0;
 }
