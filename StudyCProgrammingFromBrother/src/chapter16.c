179ae8c57d1976d954b6038956fbc247affb4f41
HyunahYou <ryuh49@naver.com>
2020-01-13 02:51:40 +0900

    chapter16-2 예제2

--- StudyCProgrammingFromBrother/src/chapter16.c
+++ StudyCProgrammingFromBrother/src/chapter16.c
@@ -1,9 +1,46 @@
 #include <stdio.h>
 
 int main (void) {
-    int arr1[2][3][4];
-    double arr2[5][5][5];
-    printf("높이2, 세로3, 가로4 int형 배열: %d\n", sizeof(arr1));
-    printf("높이5, 세로5, 가로5 double형 배열: %d\n", sizeof(arr2));
+    int mean=0, i, j;
+    int record[3][3][2]={
+            {
+                    {70, 80},
+                    {94, 90},
+                    {70, 85}
+            },
+            {
+                    {83, 90},
+                    {95, 60},
+                    {90, 82}
+            },
+            {
+                    {98, 89},
+                    {99, 94},
+                    {91, 87}
+            }
+    };
+
+    for (i=0; i<3; i++) {
+        for (j=0; j<2; j++) {
+            mean+=record[0][i][j];
+        }
+    }
+    printf("A학급 전체 평균: %g\n", (double)mean/6);
+
+    mean=0;
+    for (i=0; i<3; i++) {
+        for (j=0; j<2; j++) {
+            mean+=record[1][i][j];
+        }
+    }
+    printf("B학급 전체 평균: %g\n", (double)mean/6);
+
+    mean=0;
+    for (i=0; i<3; i++) {
+        for (j=0; j<2; j++) {
+            mean+=record[2][i][j];
+        }
+    }
+    printf("C학급 전체 평균: %g\n", (double)mean/6);
     return 0;
 }
