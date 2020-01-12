da702edf51cd0370d4fb91b3f633d1da14ad2955
HyunahYou <ryuh49@naver.com>
2020-01-13 02:41:23 +0900

    chapter16-1 문제16-1 Q3

--- StudyCProgrammingFromBrother/src/chapter16.c
+++ StudyCProgrammingFromBrother/src/chapter16.c
@@ -1,22 +1,46 @@
 #include <stdio.h>
 
-int main (void) {
-    int A[2][4]={
-            {1, 2, 3, 4},
-            {5, 6, 7, 8}
-    };
-    int B[4][2];
+int record[5][5];
+void WriteRecord (void) {
+    int sum;
     int i, j;
-
-    for (i=0; i<2; i++) {
-        for (j=0; j<4; j++)
-            B[j][i]=A[i][j];
-    }
-
     for (i=0; i<4; i++) {
-        for (j=0; j<2; j++)
-            printf("%2d", B[i][j]);
+        sum=0;
+        printf("%d번째 학생의 성적 입력\n", i+1);
+        for (j=0; j<4; j++) {
+            printf("과목 %d: ", j+1);
+            scanf("%d", &record[i][j]);
+            sum+=record[i][j];
+        }
+        record[i][4]=sum;
+    }
+}
+
+void WriteSumRecord (void) {
+    int sum=0;
+    int i, j;
+    for (i=0; i<4; i++) {
+        sum=0;
+        for (j=0; j<4; j++) {
+            sum+=record[j][i];
+        }
+        record[4][i]=sum;
+        record[4][4]+=sum;
+    }
+}
+
+void ShowAllRecord (void) {
+    int i, j;
+    for (i=0; i<5; i++) {
+        for (j=0; j<5; j++)
+            printf("%3d ", record[i][j]);
         printf("\n");
     }
+}
+
+int main (void) {
+    WriteRecord();
+    WriteSumRecord();
+    ShowAllRecord();
     return 0;
 }
