3934d985b9b3ad604bf5777cc40f173571b863a4
HyunahYou <ryuh49@naver.com>
2020-01-13 02:02:39 +0900

    chapter16-1 예제3

--- StudyCProgrammingFromBrother/src/chapter16.c
+++ StudyCProgrammingFromBrother/src/chapter16.c
@@ -1,22 +1,13 @@
 #include <stdio.h>
 
 int main (void) {
-    int villa[4][2];
-    int popu, i, j;
+    int arr[3][2];
+    int i, j;
 
-    /* 가구별 거주인원 입력 받기*/
-    for (i=0; i<4; i++) {
+    for (i=0; i<3; i++) {
         for (j=0; j<2; j++) {
-            printf("%d층 %d호 인구수: ", i+1, j+1);
-            scanf("%d", &villa[i][j]);
+            printf("%p\n", &arr[i][j]);
         }
-    }
-    /* 빌라의 층별 인구수 출력하기 */
-    for (i=0; i<4; i++) {
-        popu=0;
-        popu+=villa[i][0];
-        popu+=villa[i][1];
-        printf("%d층 인구수: %d\n", i+1, popu);
     }
     return 0;
 }
