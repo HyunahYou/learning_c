547975104647004a6b70a6b5f7320eb88bc1fb5a
HyunahYou <ryuh49@naver.com>
2020-01-13 02:44:46 +0900

    chapter16-2 예제1

--- StudyCProgrammingFromBrother/src/chapter16.c
+++ StudyCProgrammingFromBrother/src/chapter16.c
@@ -1,46 +1,9 @@
 #include <stdio.h>
 
-int record[5][5];
-void WriteRecord (void) {
-    int sum;
-    int i, j;
-    for (i=0; i<4; i++) {
-        sum=0;
-        printf("%d번째 학생의 성적 입력\n", i+1);
-        for (j=0; j<4; j++) {
-            printf("과목 %d: ", j+1);
-            scanf("%d", &record[i][j]);
-            sum+=record[i][j];
-        }
-        record[i][4]=sum;
-    }
-}
-
-void WriteSumRecord (void) {
-    int sum=0;
-    int i, j;
-    for (i=0; i<4; i++) {
-        sum=0;
-        for (j=0; j<4; j++) {
-            sum+=record[j][i];
-        }
-        record[4][i]=sum;
-        record[4][4]+=sum;
-    }
-}
-
-void ShowAllRecord (void) {
-    int i, j;
-    for (i=0; i<5; i++) {
-        for (j=0; j<5; j++)
-            printf("%3d ", record[i][j]);
-        printf("\n");
-    }
-}
-
 int main (void) {
-    WriteRecord();
-    WriteSumRecord();
-    ShowAllRecord();
+    int arr1[2][3][4];
+    double arr2[5][5][5];
+    printf("높이2, 세로3, 가로4 int형 배열: %d\n", sizeof(arr1));
+    printf("높이5, 세로5, 가로5 double형 배열: %d\n", sizeof(arr2));
     return 0;
 }
