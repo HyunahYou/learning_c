2967d7ac0260ecc8bf2bd078e5448b9c4fc8f1a3
HyunahYou <ryuh49@naver.com>
2020-01-13 16:02:02 +0900

    chapter17-2 문제17-1

--- StudyCProgrammingFromBrother/src/chapter17.c
+++ StudyCProgrammingFromBrother/src/chapter17.c
@@ -1,11 +1,29 @@
 #include <stdio.h>
 
-int main (void) {
-    int num=100;
-    int *ptr=&num;
-    int **dptr=&ptr;
-    int ***tptr=&dptr;
+void MaxAndMin (int*arr, int size, int**mxPtr, int**mnPtr) {
+    int *max, *min;
+    max=min=&arr[0];
+    for (int i=0; i<size; i++) {
+        if (*max<arr[i])
+            max=&arr[i];
+        if (*min>arr[i])
+            min=&arr[i];
+    }
+    *mxPtr=max;
+    *mnPtr=min;
+}
 
-    printf("%d %d\n", **dptr, ***tptr);
+int main (void) {
+    int*maxPtr;
+    int*minPtr;
+    int arr[5];
+
+    for (int i=0; i<5; i++) {
+        printf("정수 입력 %d: ", i+1);
+        scanf("%d", &arr[i]);
+    }
+
+    MaxAndMin(arr, sizeof(arr)/ sizeof(int), &maxPtr, &minPtr);
+    printf("최대: %d, 최소: %d\n", *maxPtr, *minPtr);
     return 0;
 }
