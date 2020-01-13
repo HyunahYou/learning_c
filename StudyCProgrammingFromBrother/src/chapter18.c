ab08a72cb17948435fa4965c6f4fc3dc3239ea1c
HyunahYou <ryuh49@naver.com>
2020-01-13 23:43:25 +0900

    chapter18-2 예제3

--- StudyCProgrammingFromBrother/src/chapter18.c
+++ StudyCProgrammingFromBrother/src/chapter18.c
@@ -1,31 +1,19 @@
 #include <stdio.h>
 
-void ShowArr2DStyle (int(*arr)[4], int colum) {
-    int i, j;
-    for (i = 0; i < colum; i++) {
-        for (j = 0; j < 4; j++)
-            printf("%d ", arr[i][j]);
-        printf("\n");
-    }
-    printf("\n");
-}
-
-int Sum2DArr (int arr[][4], int colum) {
-    int i, j, sum=0;
-    for (i = 0; i < colum; i++) {
-        for (j = 0; j < 4; j++)
-            sum += arr[i][j];
-    }
-    return sum;
-}
-
 int main (void) {
-    int arr1[2][4]={1, 2, 3, 4, 5, 6, 7, 8};
-    int arr2[3][4]={1, 1, 1, 1, 3, 3, 3, 3, 5, 5, 5, 5};
+    int a[3][2]={{1, 2}, {3, 4}, {5, 6}};
 
-    ShowArr2DStyle(arr1, sizeof(arr1)/sizeof(arr1[0]));
-    ShowArr2DStyle(arr2, sizeof(arr2)/sizeof(arr2[0]));
-    printf("arr1의 합: %d\n", Sum2DArr(arr1, sizeof(arr1)/sizeof(arr1[0])));
-    printf("arr2의 합: %d\n", Sum2DArr(arr2, sizeof(arr2)/sizeof(arr2[0])));
+    printf("a[0]: %p\n", a[0]);
+    printf("*(a+0): %p\n", *(a+0));
+
+    printf("a[1]: %p\n", a[1]);
+    printf("*(a+1): %p\n", *(a+1));
+
+    printf("a[2]: %p\n", a[2]);
+    printf("*(a+2): %p\n", *(a+2));
+
+    printf("%d, %d\n", a[2][1], (*(a+2))[1]);
+    printf("%d, %d\n", a[2][1], *(a[2]+1));
+    printf("%d, %d\n", a[2][1], *(*(a+2)+1));
     return 0;
 }
