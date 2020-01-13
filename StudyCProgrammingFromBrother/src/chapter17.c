66eac6f1a680778d1d613b7ca72a461b62a58c64
HyunahYou <ryuh49@naver.com>
2020-01-13 15:43:02 +0900

    chapter17-2 예제1

--- StudyCProgrammingFromBrother/src/chapter17.c
+++ StudyCProgrammingFromBrother/src/chapter17.c
@@ -1,15 +1,11 @@
 #include <stdio.h>
 
 int main (void) {
-    int num1=10, num2=20, num3=30;
-    int *ptr1=&num1;
-    int *ptr2=&num2;
-    int *ptr3=&num3;
+    int num=100;
+    int *ptr=&num;
+    int **dptr=&ptr;
+    int ***tptr=&dptr;
 
-    int *ptrArr[]={ptr1, ptr2, ptr3};
-    int **dptr=ptrArr;
-
-    printf("%d %d %d\n", *(ptrArr[0]), *(ptrArr[1]), *(ptrArr[2]));
-    printf("%d %d %d\n", *(dptr[0]), *(dptr[1]), *(dptr[2]));
+    printf("%d %d\n", **dptr, ***tptr);
     return 0;
 }
