201d606b4cbe6c1de22c6d200892770f70886e29
HyunahYou <ryuh49@naver.com>
2020-01-13 22:45:02 +0900

    chapter18-1 예제3

--- StudyCProgrammingFromBrother/src/chapter18.c
+++ StudyCProgrammingFromBrother/src/chapter18.c
@@ -1,14 +1,38 @@
 #include <stdio.h>
 
 int main (void) {
-    int arr1[3][2];
-    int arr2[2][3];
+    int arr1[2][2]={
+            {1, 2},
+            {3, 4}
+    };
+    int arr2[3][2]={
+            {1, 2},
+            {3, 4},
+            {5, 6}
+    };
+    int arr3[4][2]={
+            {1, 2},
+            {3, 4},
+            {5, 6},
+            {7, 8}
+    };
 
-    printf("arr1: %p\n", arr1);
-    printf("arr1+1: %p\n", arr1+1);
-    printf("arr1+2: %p\n\n", arr1+2);
+    int (*ptr)[2];
+    int i;
 
-    printf("arr2: %p\n", arr2);
-    printf("arr2+1: %p\n", arr2+1);
+    ptr=arr1;
+    printf("** Show 2,2 arr1 **\n");
+    for (i=0; i<2; i++)
+        printf("%d %d\n", ptr[i][0], ptr[i][1]);
+
+    ptr=arr2;
+    printf("** Show 3,2 arr2 **\n");
+    for (i=0; i<3; i++)
+        printf("%d %d\n", ptr[i][0], ptr[i][1]);
+
+    ptr=arr3;
+    printf("**  Show 4,2 arr3 **\n");
+    for (i=0; i<4; i++)
+        printf("%d %d\n", ptr[i][0], ptr[i][1]);
     return 0;
 }
