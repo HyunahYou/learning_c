607c3141496311b839981324c630634a015fad7d
HyunahYou <ryuh49@naver.com>
2020-01-13 22:22:01 +0900

    chapter18-1 예제2

--- StudyCProgrammingFromBrother/src/chapter18.c
+++ StudyCProgrammingFromBrother/src/chapter18.c
@@ -1,20 +1,14 @@
 #include <stdio.h>
 
 int main (void) {
-    int arr2d[3][3];
-    printf("%d\n", arr2d);
-    printf("%d\n", arr2d[0]);
-    printf("%d\n\n", &arr2d[0][0]);
+    int arr1[3][2];
+    int arr2[2][3];
 
-    printf("%d\n", arr2d[1]);
-    printf("%d\n\n", &arr2d[1][0]);
+    printf("arr1: %p\n", arr1);
+    printf("arr1+1: %p\n", arr1+1);
+    printf("arr1+2: %p\n\n", arr1+2);
 
-    printf("%d\n", arr2d[2]);
-    printf("%d\n\n", &arr2d[2][0]);
-
-    printf("sizeof(arr2d): %d\n", sizeof(arr2d));
-    printf("sizeof(arr2d[0]): %d\n", sizeof(arr2d[0]));
-    printf("sizeof(arr2d[1]: %d\n", sizeof(arr2d[1]));
-    printf("sizeof(arr2d[2]: %d\n", sizeof(arr2d[2]));
+    printf("arr2: %p\n", arr2);
+    printf("arr2+1: %p\n", arr2+1);
     return 0;
 }
