c552182e6384177873da1afcf45d4ddee0d6b875
HyunahYou <ryuh49@naver.com>
2020-01-13 15:09:39 +0900

    chapter16-1 예제2

--- StudyCProgrammingFromBrother/src/chapter17.c
+++ StudyCProgrammingFromBrother/src/chapter17.c
@@ -1,15 +1,18 @@
 #include <stdio.h>
 
-int main (void) {
-    double num=3.14;
-    double *ptr=&num;
-    double **dptr=&ptr;
-    double *ptr2;
+void SwapIntPtr (int*p1, int *p2) {
+    int *temp=p1;
+    p1=p2;
+    p2=temp;
+}
 
-    printf("%9p %9p\n", ptr, *dptr);
-    printf("%9g %9g\n", num, **dptr);
-    ptr2=*dptr;
-    *ptr2=10.99;
-    printf("%9g %9g\n", num, **dptr);
+int main (void) {
+    int num1=10, num2=20;
+    int *ptr1, *ptr2;
+    ptr1=&num1, ptr2=&num2;
+    printf("*ptr1, *ptr2: %d, %d\n", *ptr1, *ptr2);
+
+    SwapIntPtr(ptr1, ptr2);
+    printf("*ptr1, *ptr2: %d, %d\n", *ptr1, *ptr2);
     return 0;
 }
