21a78e3b9b9ad5f5092eb1f1d3b2ab64600a2093
HyunahYou <ryuh49@naver.com>
2020-01-13 23:42:27 +0900

    chapter18-2 예제2

--- StudyCProgrammingFromBrother/src/chapter18.c
+++ StudyCProgrammingFromBrother/src/chapter18.c
@@ -1,18 +1,31 @@
 #include <stdio.h>
 
-int main (void) {
-    int num1=10, num2=20, num3=30, num4=40;
-    int arr2d[2][4]={1, 2, 3, 4, 5, 6, 7, 8};
+void ShowArr2DStyle (int(*arr)[4], int colum) {
     int i, j;
-
-    int*whoA[4]={&num1, &num2, &num3, &num4};
-    int (*whoB)[4]=arr2d;
-
-    printf("%d %d %d %d\n", *whoA[0], *whoA[1], *whoA[2], *whoA[3]);
-    for (i=0; i<2; i++) {
-        for (j=0; j<4; j++)
-            printf("%d ", whoB[i][j]);
+    for (i = 0; i < colum; i++) {
+        for (j = 0; j < 4; j++)
+            printf("%d ", arr[i][j]);
         printf("\n");
     }
+    printf("\n");
+}
+
+int Sum2DArr (int arr[][4], int colum) {
+    int i, j, sum=0;
+    for (i = 0; i < colum; i++) {
+        for (j = 0; j < 4; j++)
+            sum += arr[i][j];
+    }
+    return sum;
+}
+
+int main (void) {
+    int arr1[2][4]={1, 2, 3, 4, 5, 6, 7, 8};
+    int arr2[3][4]={1, 1, 1, 1, 3, 3, 3, 3, 5, 5, 5, 5};
+
+    ShowArr2DStyle(arr1, sizeof(arr1)/sizeof(arr1[0]));
+    ShowArr2DStyle(arr2, sizeof(arr2)/sizeof(arr2[0]));
+    printf("arr1의 합: %d\n", Sum2DArr(arr1, sizeof(arr1)/sizeof(arr1[0])));
+    printf("arr2의 합: %d\n", Sum2DArr(arr2, sizeof(arr2)/sizeof(arr2[0])));
     return 0;
 }
