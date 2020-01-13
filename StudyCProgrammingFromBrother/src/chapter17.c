ec521439efa49d5465e3156fca628f5693fd8546
HyunahYou <ryuh49@naver.com>
2020-01-13 15:40:07 +0900

    chapter17-1 예제4

--- StudyCProgrammingFromBrother/src/chapter17.c
+++ StudyCProgrammingFromBrother/src/chapter17.c
@@ -1,18 +1,15 @@
 #include <stdio.h>
 
-void SwapIntPtr (int**dp1, int **dp2) {
-    int *temp=*dp1; //*temp=ptr1;
-    *dp1=*dp2;      //ptr1=ptr2;
-    *dp2=temp;      //ptr2=temp;
-}
-
 int main (void) {
-    int num1=10, num2=20;
-    int *ptr1, *ptr2;
-    ptr1=&num1, ptr2=&num2;
-    printf("*ptr1, *ptr2: %d, %d\n", *ptr1, *ptr2);
+    int num1=10, num2=20, num3=30;
+    int *ptr1=&num1;
+    int *ptr2=&num2;
+    int *ptr3=&num3;
 
-    SwapIntPtr(&ptr1, &ptr2);
-    printf("*ptr1, *ptr2: %d, %d\n", *ptr1, *ptr2);
+    int *ptrArr[]={ptr1, ptr2, ptr3};
+    int **dptr=ptrArr;
+
+    printf("%d %d %d\n", *(ptrArr[0]), *(ptrArr[1]), *(ptrArr[2]));
+    printf("%d %d %d\n", *(dptr[0]), *(dptr[1]), *(dptr[2]));
     return 0;
 }
