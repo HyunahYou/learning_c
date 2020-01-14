9fb9b0f6df72c163d33f52613c0148242a0ad943
HyunahYou <ryuh49@naver.com>
2020-01-14 21:34:19 +0900

    chapter19-2 예제1

--- StudyCProgrammingFromBrother/src/chapter19.c
+++ StudyCProgrammingFromBrother/src/chapter19.c
@@ -1,17 +1,10 @@
 #include <stdio.h>
 
-void SoSimpleFunc (void) {
-    printf("I'm so simple");
-}
+int main (int argc, char*argv[]) {
+    int i=0;
+    printf("전달된 문자열의 수: %d\n", argc);
 
-int main (void) {
-    int num=20;
-    void*ptr;
-
-    ptr=&num;
-    printf("%p\n", ptr);
-
-    ptr=SoSimpleFunc;
-    printf("%p\n", ptr);
+    for (i=0; i<argc; i++)
+        printf("%d번째 문자열: %s\n", i+1, argv[i]);
     return 0;
 }
