fb77365bd93f5a41dd77e280064ecf4937c7c227
HyunahYou <ryuh49@naver.com>
2020-01-14 21:13:22 +0900

    chapter19-1 예제3

--- StudyCProgrammingFromBrother/src/chapter19.c
+++ StudyCProgrammingFromBrother/src/chapter19.c
@@ -1,38 +1,17 @@
 #include <stdio.h>
 
-int WhoIsFirst (int age1, int age2, int (*cmp)(int n1, int n2)) {
-    return cmp(age1, age2);
-}
-
-int OlderFirst (int age1, int age2) {
-    if (age1>age2)
-        return age1;
-    else if (age1<age2)
-        return age2;
-    else
-        return 0;
-}
-
-int YoungerFirst (int age1, int age2) {
-    if (age1<age2)
-        return age1;
-    else if (age1>age2)
-        return age2;
-    else
-        return 0;
+void SoSimpleFunc (void) {
+    printf("I'm so simple");
 }
 
 int main (void) {
-    int age1=20;
-    int age2=30;
-    int first;
+    int num=20;
+    void*ptr;
 
-    printf("입장순서 1\n");
-    first=WhoIsFirst(age1, age2, OlderFirst);
-    printf("%d세와 %d세 중 %d세가 먼저 입장!\n\n", age1, age2, first);
+    ptr=&num;
+    printf("%p\n", ptr);
 
-    printf("입장순서 2\n");
-    first=WhoIsFirst(age1, age2, YoungerFirst);
-    printf("%d세와 %d세 중 %d세가 먼저 입장!\n\n", age1, age2, first);
+    ptr=SoSimpleFunc;
+    printf("%p\n", ptr);
     return 0;
 }
