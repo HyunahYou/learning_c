3d57e77c595e5369b880d7faf0650e4214f9fa30
HyunahYou <ryuh49@naver.com>
2020-01-14 22:20:21 +0900

    chapter19-2 예제3

--- StudyCProgrammingFromBrother/src/chapter19.c
+++ StudyCProgrammingFromBrother/src/chapter19.c
@@ -1,17 +1,12 @@
 #include <stdio.h>
 
-void ShowAllString (int argc, char*argv[]) {
-    int i;
-    for(i=0; i<argc; i++)
-        printf("%s\n", argv[i]);
-}
+int main (int argc, char*argv[]) {
+    int i=0;
+    printf("전달된 문자열의 수: %d\n", argc);
 
-int main (void) {
-    char*str[3]={
-            "C Programming",
-            "C++ Programming",
-            "JAVA Programmin"
-    };
-    ShowAllString(3, str);
+    while (argv[i]!=NULL) {
+        printf("%d번째 문자열: %s\n", i+1, argv[i]);
+        i++;
+    }
     return 0;
 }
