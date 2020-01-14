af236f9d6a6d8ed1b9e5ea4f4cee36b1912fbc41
HyunahYou <ryuh49@naver.com>
2020-01-14 21:05:02 +0900

    chapter19-1 예2

--- StudyCProgrammingFromBrother/src/chapter19.c
+++ StudyCProgrammingFromBrother/src/chapter19.c
@@ -1,22 +1,38 @@
 #include <stdio.h>
 
-void SimpleAdder (int n1, int n2) {
-    printf("%d+%d=%d\n", n1, n2, n1+n2);
+int WhoIsFirst (int age1, int age2, int (*cmp)(int n1, int n2)) {
+    return cmp(age1, age2);
 }
 
-void ShowString (char*str){
-    printf("%s\n", str);
+int OlderFirst (int age1, int age2) {
+    if (age1>age2)
+        return age1;
+    else if (age1<age2)
+        return age2;
+    else
+        return 0;
+}
+
+int YoungerFirst (int age1, int age2) {
+    if (age1<age2)
+        return age1;
+    else if (age1>age2)
+        return age2;
+    else
+        return 0;
 }
 
 int main (void) {
-    char*str="Function Pointer";
-    int num1=10, num2=20;
+    int age1=20;
+    int age2=30;
+    int first;
 
-    void (*fptr1)(int, int)=SimpleAdder;
-    void (*fptr2)(char*)=ShowString;
+    printf("입장순서 1\n");
+    first=WhoIsFirst(age1, age2, OlderFirst);
+    printf("%d세와 %d세 중 %d세가 먼저 입장!\n\n", age1, age2, first);
 
-    /* 함수 포인터 변수에 의한 호출 */
-    fptr1(num1, num2);
-    fptr2(str);
+    printf("입장순서 2\n");
+    first=WhoIsFirst(age1, age2, YoungerFirst);
+    printf("%d세와 %d세 중 %d세가 먼저 입장!\n\n", age1, age2, first);
     return 0;
 }
