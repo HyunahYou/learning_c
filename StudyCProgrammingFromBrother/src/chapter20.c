8878268f38f2b19ed47953967f754c6ab1b763d7
HyunahYou <ryuh49@naver.com>
2020-01-15 15:20:34 +0900

    chapter20 도전6(ing)

--- StudyCProgrammingFromBrother/src/chapter20.c
+++ StudyCProgrammingFromBrother/src/chapter20.c
@@ -1,73 +1,52 @@
-/* 도전5. 가위바위보 */
+/* 도전6. 야구게임 */
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 
 int main (void) {
-    int num, t;
-    int win=0, tie=0;
+    int i=0, strike = 0, ball = 0;
+    int t[3], k[3];
 
-    printf("바위는 1, 가위는 2, 보는 3: ");
-    scanf("%d", &num);
-    srand((int)time(NULL));
-    for (int i=0; i<10;) {
-        t = rand();
-        if (t > 0 && t < 4) {
-            if (num == 1) {
-                if (t == 1) {
-                    printf("당신은 바위 선택, 컴퓨터는 바위 선택, 비겼습니다!\n");
-                    tie++;
-                    i++;
-                    printf("바위는 1, 가위는 2, 보는 3: ");
-                    scanf("%d", &num);
-                } else if (t == 2) {
-                    printf("당신은 바위 선택, 컴퓨터는 가위 선택, 이겼습니다!\n");
-                    win++;
-                    i++;
-                    printf("바위는 1, 가위는 2, 보는 3: ");
-                    scanf("%d", &num);
-                } else if (t == 3) {
-                    printf("당신은 바위 선택, 컴퓨터는 보 선택, 졌습니다!\n");
-                    break;
-                }
-            } else if (num == 2) {
-                if (t == 1) {
-                    printf("당신은 가위 선택, 컴퓨터는 바위 선택, 졌습니다!\n");
-                    break;
-                } else if (t == 2) {
-                    printf("당신은 가위 선택, 컴퓨터는 가위 선택, 비겼습니다!\n");
-                    tie++;
-                    i++;
-                    printf("바위는 1, 가위는 2, 보는 3: ");
-                    scanf("%d", &num);
-                } else if (t == 3) {
-                    printf("당신은 가위 선택, 컴퓨터는 보 선택, 이겼습니다!\n");
-                    win++;
-                    i++;
-                    printf("바위는 1, 가위는 2, 보는 3: ");
-                    scanf("%d", &num);
-                }
-            } else if (num == 3) {
-                if (t == 1) {
-                    printf("당신은 보 선택, 컴퓨터는 바위 선택, 이겼습니다!\n");
-                    win++;
-                    i++;
-                    printf("바위는 1, 가위는 2, 보는 3: ");
-                    scanf("%d", &num);
-                } else if (t == 2) {
-                    printf("당신은 보 선택, 컴퓨터는 가위 선택, 졌습니다!\n");
-                    break;
-                } else if (t == 3) {
-                    printf("당신은 보 선택, 컴퓨터는 보 선택, 비겼습니다!\n");
-                    tie++;
-                    i++;
-                    printf("바위는 1, 가위는 2, 보는 3: ");
-                    scanf("%d", &num);
-                }
-            }
-        }
-        else continue;
+    printf("Start Game!\n");
+
+    srand((int) time(NULL));
+
+    for (i = 0; i < 3;) {
+        if (0 <= t[i] && t[i] < 10) {
+            t[i] = rand();
+            i++;
+        } else continue;
     }
-    printf("\n게임의 결과: %d승, %d무\n", win, tie);
+
+    for (i=0; i<20;) {
+        strike=0;
+        ball=0;
+        printf("%d번째 도전\n", i+1);
+        for (int l=0; l<3; l++) {
+            printf("3개의 숫자 중 %d번째 숫자 선택: ", l+1);
+            scanf("%d", &k[l]);
+        }
+
+        if (t[0]==k[0] && t[0]==k[1] && t[0]==k[2])
+            if (t[0]==k[0])
+                strike++;
+            else ball++;
+        if (t[1]==k[0] && t[1]==k[1] && t[1]==k[2])
+            if (t[1]==k[1])
+                strike++;
+            else ball++;
+        if (t[2]==k[0] && t[2]==k[1] && t[2]==k[2])
+            if (t[2]==k[2])
+                strike++;
+            else ball++;
+
+        printf("%d번째 도전 결과: %d strike, %d ball!\n", i+1, strike, ball);
+        i++;
+
+        if (strike==3) {
+            printf("Game Over!");
+            break;
+        }
+    }
     return 0;
 }
